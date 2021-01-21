908f2b6f6f86f80c7943ecfa21994ebe03b035f5
Daniel Kåre Kristensen <dani9241@elev.techcollege.dk>
2021-01-21 09:27:50 +0100

    Add project files.

---
+++ EFCoreAzureCosmos/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EFCoreAzureCosmos
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseCosmos(
+                       "https://localhost:8081",
+                       "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                       databaseName: "OrdersDB");
+        }
+    }
+}
